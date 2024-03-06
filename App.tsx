b14aad586ed0a01865213d36428a537a5e64c712
Yusuf Kartal <yusuf.kartal@avkaryazilim.com.tr>
2024-03-06 23:53:34 +0300

    Karekod için kamera eklendi, sunucuya istek atılması sağlandı

--- App.tsx
+++ App.tsx
@@ -1,56 +1,105 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NativeBaseProvider } from "native-base";
-import React from "react";
-import "react-native-gesture-handler";
-import MainPage from "./src/containers/drawer/drawer";
-import EventPage from "./src/containers/eventPage/eventPage";
-import ResetPasswordPage from "./src/containers/resetPasswordPage/resetPasswordPage";
-import SignInPage from "./src/containers/signInPage/signInPage";
-import SignUpPage from "./src/containers/signUpPage/signUpPage";
-import BuyingTickets from "./src/containers/buyingTickets/buyingTickets";
-import { AppProvider } from "./src/context/AppContext";
-import { UserProvider } from "./src/context/UserContext";
-const Stack = createStackNavigator();
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import axios from 'axios';
 
-function App() {
-	return (
-		<NativeBaseProvider>
-			<AppProvider>
-				<UserProvider>
-					<NavigationContainer>
-						<Stack.Navigator initialRouteName="SignIn">
-						<Stack.Screen
-								name="SignIn"
-								component={SignInPage}
-								options={{ headerShown: false }}
-							/>
-							<Stack.Screen
-								name="SignUp"
-								component={SignUpPage}
-								options={{ headerShown: false }}
-							/>
-							<Stack.Screen
-								name="MainPage"
-								component={MainPage}
-								options={{ headerShown: false }}
-							/>
-							<Stack.Screen
-								name="ResetPasswordPage"
-								component={ResetPasswordPage}
-								options={{ headerShown: false }}
-							/>
-							<Stack.Screen
-								name="BuyingTickets"
-								component={BuyingTickets}
-								options={{ title: 'Bilet Al' }}
-							/>
-						</Stack.Navigator>
-					</NavigationContainer>
-				</UserProvider>
-			</AppProvider>
-		</NativeBaseProvider>
-	);
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    	 setScanned(true);
+		 //replace string
+		 data = data.replace("8443", "8444");
+		 axios.get(data)
+		.then(response => {
+			console.log(response.data);
+		})
+		.catch(error => {
+			console.error("Error sending data: ", error);
+		});
+  };
+
+  const renderCamera = () => {
+    return (
+      <View style={styles.cameraContainer}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={styles.camera}
+        />
+      </View>
+    );
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Kamera izni verilmedi.</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Konser Alanına hoş geldiniz!</Text>
+      <Text style={styles.paragraph}>Bilette yer alan Karekodu okutunuz</Text>
+      {renderCamera()}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setScanned(false)}
+      >
+        <Text style={styles.buttonText}>Bilette yer alan Karekodu okutunuz</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  paragraph: {
+    fontSize: 16,
+    marginBottom: 40,
+  },
+  cameraContainer: {
+    width: '80%',
+    aspectRatio: 1,
+    overflow: 'hidden',
+    borderRadius: 10,
+    marginBottom: 40,
+  },
+  camera: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: 'blue',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
